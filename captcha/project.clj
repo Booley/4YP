3cb62db928802a82fc1f9ac6a8cfa69bb8bde78c
Bo Moon <moonbh10@gmail.com>
2016-03-07 13:14:41 +0000

    added rest of files

---
+++ captcha/project.clj
@@ -0,0 +1,16 @@
+(defproject captcha "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [anglican "0.8.0-SNAPSHOT-RMH"]
+                 [net.mikera/core.matrix "0.46.0"]
+                 [net.mikera/core.matrix.stats "0.5.0"]
+                 [net.mikera/vectorz-clj "0.37.0"]
+                 [opencv/opencv "2.4.12"]
+                 [opencv/opencv-native "2.4.12"]]
+  :main ^:skip-aot captcha.core
+  :target-path "target/%s"
+  :java-source-paths ["../CaptchaTools/src/"]
+  :profiles {:uberjar {:aot :all}})
