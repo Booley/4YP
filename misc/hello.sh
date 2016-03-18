28f7d6321adf31bfce0a87327df849f2539bb51d
Bo Moon <moonbh10@gmail.com>
2016-03-18 12:41:52 +0000

    fixed sh

--- misc/hello.sh
+++ misc/hello.sh
@@ -6,11 +6,10 @@
 #PBS -m abe
 #PBS -q parallel
 
-
 # Set output and error directories
 #PBS -o localhost:/scratch/bhmoon/Documents/Reports/results_${PBS_JOBID}.out
 #PBS -e localhost:/scratch/bhmoon/Documents/Reports/results_${PBS_JOBID}.err
-cd /scratch/bhmoon/4YP/misc
 
+cd /scratch/bhmoon/4YP/misc
 javac HelloWorld.java
 java HelloWorld
