1949a243f066622a552d229f4769d7c7a46d0f2d
Bo Moon <moonbh10@gmail.com>
2016-03-18 12:16:59 +0000

    added sample cluster job

---
+++ misc/hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directives
+#PBS -N sample-job
+#PBS -M bo.moon@st-annes.ox.ac.uk
+#PBS -m abe
+#PBS -q parallel
+
+
+# Set output and error directories
+#PBS -o localhost:/scratch/bhmoon/Documents/Reports/results_${PBS_JOBID}.out
+#PBS -e localhost:/scratch/bhmoon/Documents/Reports/results_${PBS_JOBID}.err
+cd /scratch/bhmoon/misc
+
+javac HelloWorld.java
+java HelloWorld
