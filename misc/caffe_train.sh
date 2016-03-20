0af9c1730958a53bdb870e9ea9b2411f34f1968a
bo moon <bhmoon@robots.ox.ac.uk>
2016-03-20 22:55:43 +0000

    fixed pathgit add caffe_train.sh !

--- misc/caffe_train.sh
+++ misc/caffe_train.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-# Directives
 #PBS -N caffe-train-ocr
 #PBS -M bo.moon@st-annes.ox.ac.uk
 #PBS -m abe
 #PBS -q parallel
-
-# Set output and error directories
+#PBS -V
 #PBS -o localhost:/scratch/bhmoon/Documents/Reports/caffe_train/results_${PBS_JOBID}.out
 #PBS -e localhost:/scratch/bhmoon/Documents/Reports/caffe_train/results_${PBS_JOBID}.err
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib:/scratch/bhmoon/Documents/libs/lib/x86_64-linux-gnu:/scratch/bhmoon/Documents/libs/lib/atlas-base/:/scratch/bhmoon/Documents/libs/lib/
+
 
 cd /scratch/bhmoon/4YP/CaptchaTools/models
-
 /scratch/bhmoon/caffe/build/tools/caffe.bin train -solver=letter_solver.prototxt
